4fe9ac0bb07f16a3939cf85a8a95869b8132ec54
Ben Lovell <ben.j.lovell@gmail.com>
2016-03-17 16:20:08 +0100

    Cleaning up syntax in configuration

--- configuration.nix
+++ configuration.nix
@@ -14,12 +14,16 @@
     ];
 
 
-  #i wonder if i'll regret this
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.systemWide = true;
-  hardware.pulseaudio.support32Bit = true;
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-  hardware.bluetooth.enable = true;
+  hardware = {
+    # i wonder if i'll regret this
+    pulseaudio = {
+      enable = true;
+      systemWide = true;
+      support32Bit = true;
+      package = pkgs.pulseaudioFull;
+    };
+    bluetooth.enable = true;
+  };
 
   networking.hostName = "benixos"; # Define your hostname.
   #networking.enableB43Firmware = true;
@@ -34,41 +38,41 @@
 
 
   # List services that you want to enable:
-
+  services = {
   # why not?
-  #services.dbus.enable = true;
+  #dbus.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
 
-  # run updatedb every night so locate works  
-  services.locate.enable = true;
+    # run updatedb every night so locate works  
+    locate.enable = true;
 
-  # puts browser profiles into RAM, preventing a lot of read/write to USB
-  services.psd.enable = true;
-  services.psd.users = ["ben"];
+    # puts browser profiles into RAM, preventing a lot of read/write to USB
+    psd.enable = true;
+    psd.users = ["ben"];
 
-  services.acpid.enable = true;
-  services.udev.extraRules = ''
-    KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/ben/.Xauthority", RUN+="${pkgs.stdenv.shell} -c '/home/ben/.screenlayout/auto.sh'"
-  ''
-  ;
-  #
+    acpid.enable = true;
+    udev.extraRules = ''
+      KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/ben/.Xauthority", RUN+="${pkgs.stdenv.shell} -c '/home/ben/.screenlayout/auto.sh'"
+      ''
+      ;
 
-  services.redshift = {
-    enable = true;
+    redshift = {
+      enable = true;
 
-    #Berlin
-    latitude = "52.31";
-    longitude = "13.22";
+      #Berlin
+      latitude = "52.31";
+      longitude = "13.22";
+    };
+
+    avahi.enable = true;
+    avahi.ipv6 = true;
+    avahi.nssmdns = true;
   };
-
-  services.avahi.enable = true;
-  services.avahi.ipv6 = true;
-  services.avahi.nssmdns = true;
 
   security.sudo.enable = true;
 
