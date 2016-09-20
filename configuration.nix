d044751586f25025d7e4530ac514321e6d4fb2cf
Ben Lovell <ben.j.lovell@gmail.com>
2016-09-20 22:35:36 +0200

    Now using NixOS release 16.09

--- configuration.nix
+++ configuration.nix
@@ -123,5 +123,5 @@
   virtualisation.docker.enable = true;
 
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "16.03";
+  system.stateVersion = "16.09";
 }
