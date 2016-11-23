2110eaf5b559e067ea86704f2f71fd558240cb6d
Ben Lovell <ben.j.lovell@gmail.com>
2016-11-23 12:20:10 +0100

    I think this was actually causing problems with wake up and sleep, but I'm not so sure. SD Card Reader still broken. I need a Linux based device

--- macbookpro.nix
+++ macbookpro.nix
@@ -17,5 +17,8 @@
     options snd_hda_intel index=0 model=intel-mac-auto id=PCH
     options snd_hda_intel index=1 model=intel-mac-auto id=HDMI
     options hid_apple fnmode=2
+
+    # Reset XHCI USB devices on suspend/resume, fixes SD Card reader vanishing after suspend
+    #options xhci_hcd quirks=0x80
     '';
 }
