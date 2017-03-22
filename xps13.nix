5e086d1b477e1cd69b9cff856288474424996cd4
Ben Lovell <ben.j.lovell@gmail.com>
2017-03-22 18:43:03 +0100

    Forgot to add the xps13.nix file

---
+++ xps13.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+
+{
+    # Use the gummiboot efi boot loader.
+    # n.b. no GRUB settings like everyone tells you to do
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+   # hardware.facetimehd.enable = true;
+
+    #networking.enableB43Firmware = true;
+    networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+   # #to get the sound card to work
+   # boot.extraModprobeConfig = ''
+   # options snd_hda_intel index=0 model=intel-mac-auto id=PCH
+   # options snd_hda_intel index=1 model=intel-mac-auto id=HDMI
+   # options hid_apple fnmode=2
+
+   # # Reset XHCI USB devices on suspend/resume, fixes SD Card reader vanishing after suspend
+   # #options xhci_hcd quirks=0x80
+   # '';
+}
