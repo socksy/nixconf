c5ffc48408450ccc4a1a657b1087bad973493bc1
Ben Lovell <ben.j.lovell@gmail.com>
2017-06-30 19:54:28 +0200

    Only use one xmonad

--- packages.nix
+++ packages.nix
@@ -5,10 +5,7 @@
   # $ nix-env -qaP | grep wget
    environment.systemPackages = with pkgs; [
      # X/GUI stuff
-     #haskellPackages.xmonadContrib
-     #haskellPackages.xmonadExtras
      xorg.xmodmap
-     #xmonad-with-packages
      arandr
      autorandr
      conky
