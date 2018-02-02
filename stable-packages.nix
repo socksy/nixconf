abc7b307a7fbc35583f5453596fc0d989cd7bb90
Ben Lovell <ben.j.lovell@gmail.com>
2018-02-02 16:48:36 +0100

    Unused stable-packages file to try to have packages from different nix repos

---
+++ stable-packages.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ...}:
+let 
+  stable = import <stable> {};
+in {
+  environment.systemPackages = [ 
+  #put stable packages here
+   # stable.haskellPackages.xmonad 
+   # stable.haskellPackages.yeganesh 
+    ];
+}
+
