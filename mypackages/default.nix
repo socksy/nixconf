21da7363dd6a320a0820feb3b0d7451960c31fa5
Ben Lovell <ben.j.lovell@gmail.com>
2020-03-14 20:42:37 +0100

    Not sure why I added efivar override lol

--- mypackages/default.nix
+++ mypackages/default.nix
@@ -5,6 +5,18 @@ rec {
   bluez = pkgs.bluez5;
   docker = pkgs.docker-edge;
 
+
+  efivar = pkgs.efivar.overrideDerivation (finalAttrs: rec {
+    version = "37";
+    src = pkgs.fetchFromGitHub {
+      owner = "rhinstaller";
+      repo = "efivar";
+      rev = version;
+      sha256 = "1z2dw5x74wgvqgd8jvibfff0qhwkc53kxg54v12pzymyibagwf09";
+      # date = 2018-12-05T11:17:42-05:00;
+    };
+  });
+
   keepass = pkgs.keepass.override {
     plugins = [pkgs.keepass-keefox];
   };
