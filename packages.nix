3f405062c313c2f827fbbe56dcb8e2e3891b4650
Ben Lovell <ben.j.lovell@gmail.com>
2022-08-06 14:49:04 +0200

    Update packages

--- packages.nix
+++ packages.nix
@@ -7,8 +7,10 @@
     # X/GUI stuff
     xorg.xmodmap
     arandr
+    audacity
     autorandr
     blueman
+    cachix
     conky
     compton
     dmenu
@@ -17,16 +19,18 @@
     gnome2.gnome_icon_theme
     # 16.09
     #gnome.gnomeicontheme
-    gnome2.gtk
-    gnome3.adwaita-icon-theme
-    gnome3.gtk
-    gnome3.simple-scan
+    gtk2
+    gnome.adwaita-icon-theme
+    gtk3
+    simple-scan
     haskellPackages.xmonad
     haskellPackages.yeganesh
     libnotify
     lxappearance
     numix-gtk-theme
     numix-icon-theme-circle
+    #ocenaudio #audacity alternative
+    rofi #better dmenu
     xbrightness
     xcalib
     xcape
@@ -59,10 +63,9 @@
     jmtpfs # for mtp access with phones
     lshw
     nfs-utils
-    p7zip
+    #p7zip
     pciutils
     powertop
-    rfkill
     smartmontools
     tarsnap
     #udev #breaks config with list->string type errors now?
@@ -73,17 +76,17 @@
     asciinema
     aspell
     aspellDicts.en
-    awscli
+    awscli2
     bc
     binutils
     byzanz
     #chrpath
     cowsay
-    direnv
+    direnv #better dmenu
     encfs
     elinks
     feh
-    ffmpeg-full
+    #ffmpeg-full
     fzf
     graphicsmagick
     ghostscript
@@ -97,13 +100,13 @@
     magic-wormhole
     manpages
     mosh
+    mpv
     #ngrok
     nmap
     openssl
     openvpn
     pandoc
     pavucontrol
-    python27Packages.pkgconfig
     psmisc
     pv
     ripgrep
@@ -129,23 +132,31 @@
     #dwb
     firefox-wrapper
     google-chrome
+    midori
+    #luakit
 
     # gui utils
+    alacritty
     anki
     baobab
+    blender
     #calibre
     #dropbox
     evince
     evtest
+    #discord
     dpkg
     gimp
     gparted
     gpicview
-    gnome3.cheese
-    gnome3.gnome-font-viewer
+    gnome.cheese
+    gnome.gnome-font-viewer
+    helm
     inkscape
+    #kdenlive
     keepassxc
     libreoffice
+    peek # gif screengrabs
     pinta
     #qbittorrent
     qjackctl
@@ -154,75 +165,82 @@
     #skype
     slack
     spotify
-    #temporarily disabled while the build is broken, shadowing with stable.tdesktop
-    #tdesktop
+    tdesktop
     vlc
+    wire-desktop
     wpa_supplicant_gui
     xdotool
     xfce.thunar
     xfce.terminal
+    gnome.nautilus
+    gnome.sushi
     xournal
     xsane
     zoom-us
 
     # dev
     atom
+    androidenv.androidPkgs_9_0.platform-tools
     aws
     boot
     cargo
+    #clj-kondo
     clojure
+    clojure-lsp
     cmake
     compass
     ctop
     docker-edge
     docker_compose
     docker-gc
-    dust #from pixie
-    emacs
+    #emacs
+    emacsGit
     elixir
     gcc
     #ghc
     git
     gitAndTools.gitflow
+    gitAndTools.delta
     go
     gocode
     gnumake
-    j
+    # not cached rn so commenting out so as not to build from scratch
+    #graalvm8
+    #j
     jdk
     jekyll
     leiningen
     #lumo
     lua
-    #neovim
+    neovim
     nodejs-12_x
     patchelf
-    pixie
     protobuf
-    python
     python3
-    python27Packages.boto
-    python36Packages.pip
-    python36Packages.pip
-    python36Packages.virtualenv
-    python36Packages.virtualenvwrapper
+    python38Packages.pip
+    python38Packages.virtualenv
+    python38Packages.virtualenvwrapper
     R
     racket
     ruby
     rustc
     sqlite
     vimHugeX
+    zeal
 
     # misc
     mplayer
+    pulseaudio-ctl  
     # lol flashplayer is broken and no-one noticed
     #flashplayer
-    gstreamer
+    #gstreamer
     #hal-flash #DRM for flashplayer
     #wineUnstable
     #winetricks
     #pypyPackages.wxPython30
 
     #games
+    scummvm
     #openlierox
     #xonotic
     #steam
@@ -234,9 +252,6 @@
       #enableGoogleTalkPlugin = true;
       #enableAdobeFlash = true;
     };
-    emacs = {
-      withXwidgets = true;
-    };
     chromium = {
       #enablePepperFlash = true;
       #enablePepperPDF = true;
@@ -246,4 +261,11 @@
       inherit pkgs;
     };
   };
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      #url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      url = https://github.com/nix-community/emacs-overlay/archive/fe1b51ee407a6c61162477ecac84c061bc15a600.tar.gz;
+      #url = https://github.com/nix-community/emacs-overlay/archive/fa641d34da805361a033c4682ce116f0965533f4.tar.gz;
+    }))
+  ];
 }
