66c6bfd1ee0c9580e03a820961a57c70ee219e9d
Ben Lovell <ben.j.lovell@gmail.com>
2025-03-26 16:32:04 +0100

    Enable OBS & virtual camera

--- framework/configuration.nix
+++ framework/configuration.nix
@@ -310,6 +310,15 @@
     enable = true;
   };
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+    enableVirtualCamera = true;
+  };
+
   # List services that you want to enable:
 
   # ability to flash firmware updates
