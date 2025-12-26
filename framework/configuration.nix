073e0abfb0755f38a61e4bf99a442397e9f342d6
Ben Lovell <ben.j.lovell@gmail.com>
2025-12-26 21:57:14 +0100

    Add Mullvad

--- framework/configuration.nix
+++ framework/configuration.nix
@@ -88,7 +88,21 @@
     "1.0.0.1"
     "8.8.8.8"
   ];
-  services.resolved.enable = true;
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    dnsovertls = "true";
+  };
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn;
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
